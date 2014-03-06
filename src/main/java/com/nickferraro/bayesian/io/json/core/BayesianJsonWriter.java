63eca26d5a68334ab0385e33a96c50b032cdafef
Nick Ferraro <nferraro@salesforce.com>
2014-03-06 13:29:59 -0800

    Added a test-specific subclass and refactored bayesian json writer to improve testability

--- src/main/java/com/nickferraro/bayesian/io/json/core/BayesianJsonWriter.java
+++ src/main/java/com/nickferraro/bayesian/io/json/core/BayesianJsonWriter.java
@@ -76,81 +76,29 @@ public class BayesianJsonWriter implements IBayesianJsonWriter {
 			// Add the rows count attribute
 			bayesianObject.put(KEY_ROWS_COUNT, model.getTotalRows());
 
-			// Get the set of unique categories, should never be null but check for safety
-			Set<T> uniqueCategories = model.getUniqueCategories();
-			if( uniqueCategories == null ) {
-				uniqueCategories = Collections.emptySet();
-			}
+			// Create the unique categories json array
+			JSONArray uniqueCategoriesArray = writeCategoriesArray(model);
 			
 			// Add the unique categories count attribute
-			bayesianObject.put(KEY_UNIQUE_CATEGORIES_COUNT, uniqueCategories.size());
-
-			// Create the unique categories json array
-			JSONArray uniqueCategoriesArray = new JSONArray();
-			for(T uniqueCategory : uniqueCategories) {
-				// Create a single unique categorie json object
-				JSONObject categoryObject = new JSONObject();
-				
-				// Add the category name and row count attributes
-				categoryObject.put(KEY_CATEGORY, uniqueCategory.toString());
-				categoryObject.put(KEY_COUNT, model.countRowsWithCategory(uniqueCategory));
-				
-				// Add the category object to the word array
-				uniqueCategoriesArray.put(categoryObject);
-			}
+			bayesianObject.put(KEY_UNIQUE_CATEGORIES_COUNT, uniqueCategoriesArray.length());
 			
 			// Add the unique categories json array
 			bayesianObject.put(KEY_UNIQUE_CATEGORIES, uniqueCategoriesArray);
-			
-			// Get the set of unique words, should never be null but check for safety
-			Set<String> uniqueWords = model.getUniqueWords();
-			if( uniqueWords == null ) {
-				uniqueWords = Collections.emptySet();
-			}
-			
-			// Add the unique words count attribute
-			bayesianObject.put(KEY_UNIQUE_WORDS_COUNT, uniqueWords.size());
 
 			// Create the unique words json array
-			JSONArray uniqueWordsArray = new JSONArray();
-			for(String uniqueWord : uniqueWords) {
-				// Create a single unique word json object
-				JSONObject wordObject = new JSONObject();
-				
-				// Add the word name and row count attributes
-				wordObject.put(KEY_WORD, uniqueWord.toString());
-				wordObject.put(KEY_COUNT, model.countRowsWithWord(uniqueWord));
-				
-				// Add the word object to the word array
-				uniqueWordsArray.put(wordObject);
-			}
+			JSONArray uniqueWordsArray = writeWordsArray(model);
+			
+			// Add the unique words count attribute
+			bayesianObject.put(KEY_UNIQUE_WORDS_COUNT, uniqueWordsArray.length());
 			
 			// Add the unique words json array
 			bayesianObject.put(KEY_UNIQUE_WORDS, uniqueWordsArray);
 			
-			// Get the list of links, should never be null but check for safety
-			List<ILink<T>> links = model.getLinks();
-			if( links == null ) {
-				links = Collections.emptyList();
-			}
+			// Create the links json array
+			JSONArray linksArray = writeLinksArray(model);
 			
 			// Add the links count attribute
-			bayesianObject.put(KEY_LINKS_COUNT, links.size());
-			
-			// Create the links json array
-			JSONArray linksArray = new JSONArray();
-			for(ILink<T> link : links) {
-				// Create a link json object
-				JSONObject linkObject = new JSONObject();
-				
-				// Add the category, word, and weight attributes
-				linkObject.put(KEY_CATEGORY, link.getCategory().toString());
-				linkObject.put(KEY_WORD, link.getWord());
-				linkObject.put(KEY_WEIGHT, link.getWeight());
-				
-				// Add the link object to the array
-				linksArray.put(linkObject);
-			}
+			bayesianObject.put(KEY_LINKS_COUNT, linksArray.length());
 			
 			// Add the links array attribute
 			bayesianObject.put(KEY_LINKS, linksArray);
@@ -173,5 +121,77 @@ public class BayesianJsonWriter implements IBayesianJsonWriter {
 	@Override
 	public synchronized JSONObject getJsonObject() {
 		return this.bayesianObject;
+	}
+	
+	protected <T> JSONArray writeCategoriesArray(IBayesianModel<T> model) {
+		// Get the set of unique categories, should never be null but check for safety
+		Set<T> uniqueCategories = model.getUniqueCategories();
+		if( uniqueCategories == null ) {
+			uniqueCategories = Collections.emptySet();
+		}
+
+		// Create the unique categories json array
+		JSONArray uniqueCategoriesArray = new JSONArray();
+		for(T uniqueCategory : uniqueCategories) {
+			// Create a single unique categorie json object
+			JSONObject categoryObject = new JSONObject();
+			
+			// Add the category name and row count attributes
+			categoryObject.put(KEY_CATEGORY, uniqueCategory.toString());
+			categoryObject.put(KEY_COUNT, model.countRowsWithCategory(uniqueCategory));
+			
+			// Add the category object to the word array
+			uniqueCategoriesArray.put(categoryObject);
+		}
+		
+		return uniqueCategoriesArray;
+	}
+	protected <T> JSONArray writeWordsArray(IBayesianModel<T> model) {
+		// Get the set of unique words, should never be null but check for safety
+		Set<String> uniqueWords = model.getUniqueWords();
+		if( uniqueWords == null ) {
+			uniqueWords = Collections.emptySet();
+		}
+					
+		// Create the unique words json array
+		JSONArray uniqueWordsArray = new JSONArray();
+		for(String uniqueWord : uniqueWords) {
+			// Create a single unique word json object
+			JSONObject wordObject = new JSONObject();
+			
+			// Add the word name and row count attributes
+			wordObject.put(KEY_WORD, uniqueWord.toString());
+			wordObject.put(KEY_COUNT, model.countRowsWithWord(uniqueWord));
+			
+			// Add the word object to the word array
+			uniqueWordsArray.put(wordObject);
+		}
+		
+		return uniqueWordsArray;
+	}
+	
+	protected <T> JSONArray writeLinksArray(IBayesianModel<T> model) {
+		// Get the list of links, should never be null but check for safety
+		List<ILink<T>> links = model.getLinks();
+		if( links == null ) {
+			links = Collections.emptyList();
+		}
+		
+		// Create the links json array
+		JSONArray linksArray = new JSONArray();
+		for(ILink<T> link : links) {
+			// Create a link json object
+			JSONObject linkObject = new JSONObject();
+			
+			// Add the category, word, and weight attributes
+			linkObject.put(KEY_CATEGORY, link.getCategory().toString());
+			linkObject.put(KEY_WORD, link.getWord());
+			linkObject.put(KEY_WEIGHT, link.getWeight());
+			
+			// Add the link object to the array
+			linksArray.put(linkObject);
+		}
+		
+		return linksArray;
 	}
 }
--- src/test/java/com/nickferraro/bayesian/io/json/core/BayesianJsonWriterTest.java
+++ src/test/java/com/nickferraro/bayesian/io/json/core/BayesianJsonWriterTest.java
@@ -15,6 +15,7 @@ import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -31,7 +32,15 @@ public class BayesianJsonWriterTest {
 	public void setup() {
 		mockModel = mock(IBayesianModel.class); 
 		stringWriter = new StringWriter();
-		writer = new BayesianJsonWriter(stringWriter);
+		writer = new BayesianJsonWriterHelper(stringWriter);
+	}
+	
+	@After
+	public void teardown() {
+		try {
+			writer.close();
+		} catch (IOException e) {
+		}
 	}
 	
 	@Test(expected = InvalidParameterException.class)
@@ -60,32 +69,61 @@ public class BayesianJsonWriterTest {
 		
 		String jsonString = stringWriter.toString();
 		JSONObject jsonObject = new JSONObject(jsonString);
-		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_ROWS_COUNT), is(6));
-		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_UNIQUE_CATEGORIES_COUNT), is(3));
-		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_UNIQUE_WORDS_COUNT), is(3));
-		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_LINKS_COUNT), is(7));
+		assertBayesianModelJson(jsonObject);
+		assertBayesianModelJson(writer.getJsonObject());
+	}
+	
+	@Test(expected = IOException.class)
+	public void testWriteModel_WriteTwice() throws IOException {
+		Set<String> categories = createCategories();
+		Set<String> words = createWords();
+		List<ILink<String>> links = createLinks();
+		
+		when(mockModel.getUniqueCategories()).thenReturn(categories);
+		when(mockModel.getUniqueWords()).thenReturn(words);
+		when(mockModel.getLinks()).thenReturn(links);
+		when(mockModel.getTotalRows()).thenReturn(6);
+		when(mockModel.countRowsWithCategory("A")).thenReturn(2);
+		when(mockModel.countRowsWithCategory("B")).thenReturn(2);
+		when(mockModel.countRowsWithCategory("C")).thenReturn(2);
+		when(mockModel.countRowsWithWord("x")).thenReturn(3);
+		when(mockModel.countRowsWithWord("y")).thenReturn(4);
+		when(mockModel.countRowsWithWord("z")).thenReturn(3);
+		
+		writer.writeModel(mockModel);
+		writer.writeModel(mockModel);
+	}
+	
+	@Test(expected = IOException.class)
+	public void testWriteModel_NullModel() throws IOException {
+		writer.writeModel(null);
+	}
+	
+	@Test
+	public void testWriteModel_BadModel() throws IOException {
+		writer.writeModel(mockModel);
+		
+		String jsonString = stringWriter.toString();
+		JSONObject jsonObject = new JSONObject(jsonString);
+		
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_ROWS_COUNT), is(0));
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_UNIQUE_CATEGORIES_COUNT), is(0));
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_UNIQUE_WORDS_COUNT), is(0));
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_LINKS_COUNT), is(0));
 		
 		JSONArray categoriesArray = jsonObject.getJSONArray(BayesianJsonWriter.KEY_UNIQUE_CATEGORIES);
-		assertThat(categoriesArray.length(), is(3));
-		assertThat(hasCategory("A", 2, categoriesArray), is(true));
-		assertThat(hasCategory("B", 2, categoriesArray), is(true));
-		assertThat(hasCategory("C", 2, categoriesArray), is(true));
+		assertThat(categoriesArray.length(), is(0));
 		
 		JSONArray wordsArray = jsonObject.getJSONArray(BayesianJsonWriter.KEY_UNIQUE_WORDS);
-		assertThat(wordsArray.length(), is(3));
-		assertThat(hasWord("x", 3, wordsArray), is(true));
-		assertThat(hasWord("y", 4, wordsArray), is(true));
-		assertThat(hasWord("z", 3, wordsArray), is(true));
+		assertThat(wordsArray.length(), is(0));
 		
 		JSONArray linksArray = jsonObject.getJSONArray(BayesianJsonWriter.KEY_LINKS);
-		assertThat(linksArray.length(), is(7));
-		assertThat(hasLink("A", "x", 2, linksArray), is(true));
-		assertThat(hasLink("A", "y", 1, linksArray), is(true));
-		assertThat(hasLink("B", "x", 1, linksArray), is(true));
-		assertThat(hasLink("B", "y", 2, linksArray), is(true));
-		assertThat(hasLink("B", "z", 1, linksArray), is(true));
-		assertThat(hasLink("C", "y", 1, linksArray), is(true));
-		assertThat(hasLink("C", "z", 2, linksArray), is(true));
+		assertThat(linksArray.length(), is(0));
+	}
+	
+	@Test
+	public void testWriteCategories() {
+		
 	}
 	
 	private static boolean hasCategory(String category, int count, JSONArray categoryArray) {
@@ -123,6 +161,35 @@ public class BayesianJsonWriterTest {
 		}
 		
 		return false;
+	}
+	
+	private static void assertBayesianModelJson(JSONObject jsonObject) {
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_ROWS_COUNT), is(6));
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_UNIQUE_CATEGORIES_COUNT), is(3));
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_UNIQUE_WORDS_COUNT), is(3));
+		assertThat(jsonObject.getInt(BayesianJsonWriter.KEY_LINKS_COUNT), is(7));
+		
+		JSONArray categoriesArray = jsonObject.getJSONArray(BayesianJsonWriter.KEY_UNIQUE_CATEGORIES);
+		assertThat(categoriesArray.length(), is(3));
+		assertThat(hasCategory("A", 2, categoriesArray), is(true));
+		assertThat(hasCategory("B", 2, categoriesArray), is(true));
+		assertThat(hasCategory("C", 2, categoriesArray), is(true));
+		
+		JSONArray wordsArray = jsonObject.getJSONArray(BayesianJsonWriter.KEY_UNIQUE_WORDS);
+		assertThat(wordsArray.length(), is(3));
+		assertThat(hasWord("x", 3, wordsArray), is(true));
+		assertThat(hasWord("y", 4, wordsArray), is(true));
+		assertThat(hasWord("z", 3, wordsArray), is(true));
+		
+		JSONArray linksArray = jsonObject.getJSONArray(BayesianJsonWriter.KEY_LINKS);
+		assertThat(linksArray.length(), is(7));
+		assertThat(hasLink("A", "x", 2, linksArray), is(true));
+		assertThat(hasLink("A", "y", 1, linksArray), is(true));
+		assertThat(hasLink("B", "x", 1, linksArray), is(true));
+		assertThat(hasLink("B", "y", 2, linksArray), is(true));
+		assertThat(hasLink("B", "z", 1, linksArray), is(true));
+		assertThat(hasLink("C", "y", 1, linksArray), is(true));
+		assertThat(hasLink("C", "z", 2, linksArray), is(true));
 	}
 	
 	private static Set<String> createCategories() {
