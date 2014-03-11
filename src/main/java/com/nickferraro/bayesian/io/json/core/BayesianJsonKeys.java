dc27445a09e4c89930843f22feb75c00bb23c7da
Nick Ferraro <nferraro@salesforce.com>
2014-03-11 15:12:06 -0700

    Extracted bayesian json keys into their own interface file

---
+++ src/main/java/com/nickferraro/bayesian/io/json/core/BayesianJsonKeys.java
@@ -0,0 +1,15 @@
+package com.nickferraro.bayesian.io.json.core;
+
+public interface BayesianJsonKeys {
+	public static final String KEY_ROWS_COUNT = "rows-count";
+	public static final String KEY_UNIQUE_CATEGORIES_COUNT = "unique-categories-count";
+	public static final String KEY_UNIQUE_WORDS_COUNT = "unique-words-count";
+	public static final String KEY_LINKS_COUNT = "links-count";
+	public static final String KEY_UNIQUE_CATEGORIES = "unique-categories";
+	public static final String KEY_UNIQUE_WORDS = "unique-words";
+	public static final String KEY_CATEGORY = "category";
+	public static final String KEY_WORD = "word";
+	public static final String KEY_LINKS = "links";
+	public static final String KEY_WEIGHT = "weight";
+	public static final String KEY_COUNT = "count";
+}
